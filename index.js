e5c3f5239cc8d593093d7f66789008dd39738523
ralusek <ralusek@gmail.com>
2017-06-03 17:59:57 -0700

    Initialized repository.

---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const __get = require('hodash.get');
+
+
+/**
+ *
+ */
+function reshape(mapping, obj, config) {
+  config = config || {};
+
+  const output = {};
+
+  for (const key in mapping) {
+    const value = mapping[key];
+
+    let mapped;
+    // Assume .call and .apply means it's a function.
+    if (value.call && value.apply) {
+      try { mapped = value(obj); }
+      catch (err) {}
+    }
+    else if (typeof value === 'string') mapped = __get(obj, value);
+    else mapped = value;
+
+    if (mapped === undefined && config.undefinedAsNull) mapped = null;
+    output[key] = mapped;
+  }
+
+  return output;
+};
+
+
+/**
+ *
+ */
+module.exports = reshape;
